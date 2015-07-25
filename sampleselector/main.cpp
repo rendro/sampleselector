ceb4526f994e48667cd752b3e173452c825c3804
Robert Fleischmann <rendro87@gmail.com>
2015-07-25 13:49:10 +0300

    Initial Commit

---
+++ sampleselector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sampleselector
+//
+//  Created by Robert Fleischmann on 25/07/2015.
+//  Copyright (c) 2015 Robert Fleischmann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
